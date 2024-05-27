e266123e9819dab23c62f2cccebd98827bac7c40
dolf <rudolfbyker@gmail.com>
2024-05-27 08:16:04 +0200

    Don't wrap tests in `if` statements. Rather skip tests conditionally.

--- test/workaround/test_remove_atexit_permission_error.py
+++ test/workaround/test_remove_atexit_permission_error.py
@@ -8,56 +8,62 @@ import warnings
 import os
 from aa_py_openpyxl_util import remove_atexit_permission_error
 
-if os.name == "nt":
 
-    def create_subprocess_lock(filename):
-        lock_code = dedent(
-            f"""
-            import msvcrt
-            import time
-            f = open(r'{filename}', 'wb')
-            msvcrt.locking(f.fileno(), msvcrt.LK_LOCK, 0)
-            time.sleep(10)
-            """
-        )
+def create_subprocess_lock(filename):
+    lock_code = dedent(
+        f"""
+        import msvcrt
+        import time
+        f = open(r'{filename}', 'wb')
+        msvcrt.locking(f.fileno(), msvcrt.LK_LOCK, 0)
+        time.sleep(10)
+        """
+    )
 
-        subprocess.Popen([sys.executable, "-c", lock_code])
-        time.sleep(0.1)
+    subprocess.Popen([sys.executable, "-c", lock_code])
+    time.sleep(0.1)
 
-    class TestRemoveAtexitPermissionError(unittest.TestCase):
-        def test_permission_error(self) -> None:
-            with warnings.catch_warnings():
-                warnings.simplefilter("ignore", ResourceWarning)
 
-                import openpyxl.worksheet._writer
+class TestRemoveAtexitPermissionError(unittest.TestCase):
+    def test_permission_error(self) -> None:
+        if os.name != "nt":
+            self.skipTest("TODO: This test only works on Windows")
 
-                tmp = openpyxl.worksheet._writer.create_temporary_file()
-                create_subprocess_lock(tmp)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", ResourceWarning)
 
-                with self.assertRaises(PermissionError):
-                    atexit._run_exitfuncs()
-                    openpyxl.worksheet._writer._openpyxl_shutdown()
+            import openpyxl.worksheet._writer
 
-        def test_no_more_permission_error(self) -> None:
-            with warnings.catch_warnings():
-                warnings.simplefilter("ignore", ResourceWarning)
+            tmp = openpyxl.worksheet._writer.create_temporary_file()
+            create_subprocess_lock(tmp)
 
-                import openpyxl.worksheet._writer
-
-                tmp = openpyxl.worksheet._writer.create_temporary_file()
-                create_subprocess_lock(tmp)
-
-                remove_atexit_permission_error()
+            with self.assertRaises(PermissionError):
                 atexit._run_exitfuncs()
                 openpyxl.worksheet._writer._openpyxl_shutdown()
 
-        def test_only_register_once(self) -> None:
-            remove_atexit_permission_error()
-            n = atexit._ncallbacks()
-            self.assertEqual(n, atexit._ncallbacks())
+    def test_no_more_permission_error(self) -> None:
+        if os.name != "nt":
+            self.skipTest("TODO: This test only works on Windows")
+
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", ResourceWarning)
+
+            import openpyxl.worksheet._writer
+
+            tmp = openpyxl.worksheet._writer.create_temporary_file()
+            create_subprocess_lock(tmp)
 
             remove_atexit_permission_error()
-            self.assertEqual(n, atexit._ncallbacks())
+            atexit._run_exitfuncs()
+            openpyxl.worksheet._writer._openpyxl_shutdown()
 
-            remove_atexit_permission_error()
-            self.assertEqual(n, atexit._ncallbacks())
+    def test_only_register_once(self) -> None:
+        remove_atexit_permission_error()
+        n = atexit._ncallbacks()
+        self.assertEqual(n, atexit._ncallbacks())
+
+        remove_atexit_permission_error()
+        self.assertEqual(n, atexit._ncallbacks())
+
+        remove_atexit_permission_error()
+        self.assertEqual(n, atexit._ncallbacks())
