f8e84a9069068a4a3cbbd4bcff1ab430188343bb
dolf <rudolfbyker@gmail.com>
2024-11-13 13:26:34 +0200

    Refactor: Organize imports.

--- aa_py_openpyxl_util/__init__.py
+++ aa_py_openpyxl_util/__init__.py
@@ -7,6 +7,7 @@ from ._context import safe_load_workbook, changed_builtin_number_formats
 from ._extract import extract_data_from_numbered_tables, read_table, read_dict_table
 from ._find_table import find_table
 from ._iter_tables import iter_named_range_tables, iter_list_object_tables
+from ._named_ranges import define_named_ranges_for_dict_table
 from ._typing import TableCells
 from ._workarounds import save_workbook_workaround, remove_atexit_permission_error
 from ._write_only import (
@@ -15,7 +16,6 @@ from ._write_only import (
     write_tables_side_by_side,
     write_tables_side_by_side_over_multiple_sheets,
 )
-from ._named_ranges import define_named_ranges_for_dict_table
 from ._written_tables_types import (
     WrittenTables,
     WrittenTablesInSheet,
